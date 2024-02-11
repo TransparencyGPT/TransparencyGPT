3fb98f9aa3488323c5107ce7ddabb5025cee816c
Lucy Zimmerman <30590230+lucyzimmo@users.noreply.github.com>
2024-02-11 15:31:28 -0800

    updated name

--- TransparencyApp/App.js
+++ TransparencyApp/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import AnalysisComponent from "./analysiscomponent";
+import AnalysisComponent from "./claudeanalysis";
 
 export default function App() {
   return (
